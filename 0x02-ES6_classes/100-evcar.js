1ff50cbf76353d1548fba0e21447b438a704268e
Basant-Adel <basantadel99@gmail.com>
2024-01-04 23:55:11 +0000

    End Please! :D

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -3,7 +3,7 @@ import Car from './10-car';
 export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
     super(brand, motor, color);
-    this.range = range;
+    this._range = range;
   }
 
   get range() {
@@ -14,8 +14,8 @@ export default class EVCar extends Car {
     this._range = value;
   }
 
+  // eslint-disable-next-line class-methods-use-this
   cloneCar() {
-    const Species = super.constructor[Symbol.species];
-    return new Species();
+    return new Car(); // Return an instance of Car, not EVCar
   }
 }
